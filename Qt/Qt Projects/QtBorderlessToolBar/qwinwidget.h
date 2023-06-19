c5047120a9e491a4848e6a04abc4da9765f3c0b1
tongstar <tongmon@hanmail.net>
2023-06-20 06:33:41 +0900

    23/06/33 update

---
+++ Qt/Qt Projects/QtBorderlessToolBar/qwinwidget.h
@@ -0,0 +1,56 @@
+#ifndef QWINWIDGET_H
+#define QWINWIDGET_H
+
+#include <QVBoxLayout>
+#include <QWidget>
+
+#include "WinNativeWindow.h"
+#include "widget.h"
+
+class QWinWidget : public QWidget
+{
+    Q_OBJECT
+  public:
+    QWinWidget();
+    ~QWinWidget();
+
+    void show();
+    void center();
+    void showCentered();
+    void setGeometry(int x, int y, int w, int h);
+
+    HWND getParentWindow() const;
+
+  public slots:
+    void onMaximizeButtonClicked();
+    void onMinimizeButtonClicked();
+    void onCloseButtonClicked();
+
+  protected:
+    void childEvent(QChildEvent *e) override;
+    bool eventFilter(QObject *o, QEvent *e) override;
+
+    bool focusNextPrevChild(bool next) override;
+    void focusInEvent(QFocusEvent *e) override;
+
+    bool nativeEvent(const QByteArray &eventType, void *message, long *result) override;
+
+  private:
+    QVBoxLayout m_Layout;
+
+    Widget *p_Widget;
+
+    WinNativeWindow *p_ParentWinNativeWindow;
+    HWND m_ParentNativeWindowHandle;
+
+    HWND _prevFocus;
+    bool _reenableParent;
+
+    int BORDERWIDTH = 6;    // Adjust this as you wish for # of pixels on the edges to show resize handles
+    int TOOLBARHEIGHT = 40; // Adjust this as you wish for # of pixels from the top to allow dragging the window
+
+    void saveFocus();
+    void resetFocus();
+};
+
+#endif // QWINWIDGET_H
