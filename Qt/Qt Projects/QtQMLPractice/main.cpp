fd696de7dd6e18c6c17e1df055cafa055013ccbf
tongstar <68733249+tongmon@users.noreply.github.com>
2023-07-06 16:42:39 +0900

    Update main.cpp

--- Qt/Qt Projects/QtQMLPractice/main.cpp
+++ Qt/Qt Projects/QtQMLPractice/main.cpp
@@ -2,22 +2,22 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+int main(int argc, char *argv[]) {
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
-    QGuiApplication app(argc, argv);
+  QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/QmlDir/main.qml"));
-    QObject::connect(
-        &engine, &QQmlApplicationEngine::objectCreated,
-        &app, [url](QObject *obj, const QUrl &objUrl)
-        {
+  QQmlApplicationEngine engine;
+  const QUrl url(QStringLiteral("qrc:/QmlDir/main.qml"));
+  QObject::connect(
+      &engine, &QQmlApplicationEngine::objectCreated, &app,
+      [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
-            QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
-    engine.load(url);
+          QCoreApplication::exit(-1);
+      },
+      Qt::QueuedConnection);
 
-    return app.exec();
+  engine.load(url);
+
+  return app.exec();
 }
